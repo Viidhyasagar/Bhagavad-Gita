e493d236954d54c40f5bc8844401483d8118d1f2
Vidhya Veeragani <viveeragani@atd.com>
2023-08-31 15:57:00 -0700

    Code Description

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
+// This code is a simple React application that fetches Bhagavad Gita
+// Verses from a server and displays them on a webpage
+
+// !1. Import Statements:
 import React, { useState, useEffect } from 'react';
+//  It uses functional components and the useState and useEffect hooks for state management and data fetching
 import axios from 'axios';
-import './App.css';
-import { Typography, Container } from '@mui/material';
-import _ from 'lodash';
-import ReactPaginate from 'react-paginate';
 
+
+/**
+ * !2.Functional Component
+ */
 const App = () => {
-  const [verses, setVerses] = useState([]);
-  const [displayedVerses, setDisplayedVerses] = useState([]);
-  const [searchChapter, setSearchChapter] = useState('');
-  const [searchVerse, setSearchVerse] = useState('');
-  const [pageNumber, setPageNumber] = useState(0); // Track current page number
-  const versesPerPage = 10; // Number of verses per page
-  const [selectedVerse, setSelectedVerse] = useState(null); // Track selected verse
 
+  /** 
+   * !3.State Initialization
+   *  * Initialize a state variable called verses using the useState hook.
+   *  ? This variable will hold the Bhagavad Gita verses retrieved from the server.
+   */
+  const [verses, setVerses] = useState([]);
+
+  /** 
+   * !4.useEffect Hook:
+   * *Use the useEffect hook to perform side effects in the component.
+   * *In this case, it fetches data from the server when the component mounts -
+   * ?-([] as the dependency array means it runs once after the initial render).
+   */
   useEffect(() => {
+
+    /**
+     * !5.Data Fetching
+     * Define an asynchronous function fetchVerses that uses axios to make a GET request 
+     * - to 'http://localhost:8001/api/verses' to retrieve the Bhagavad Gita verses.
+     * If the request is successful, the response data is stored in the 
+     * - verses state variable using setVerses.
+     * If there's an error, it's caught and logged to the console.
+     */
     const fetchVerses = async () => {
       try {
         const response = await axios.get('http://localhost:8001/api/verses');
-        const sortedVerses = _.sortBy(response.data, ['Chapter', 'Verse']);
-        setVerses(sortedVerses);
-        setDisplayedVerses(sortedVerses);
+        setVerses(response.data);
       } catch (error) {
         console.error('Error fetching verses:', error);
       }
@@ -29,97 +47,24 @@ const App = () => {
     fetchVerses();
   }, []);
 
-  const handleChapterInputChange = (query) => {
-    setSearchChapter(query);
-  };
-
-  const handleVerseInputChange = (query) => {
-    setSearchVerse(query);
-  };
-
-  const handleSearch = () => {
-    const filteredVerses = verses.filter((verse) => {
-      const chapterAndVerse = `Chapter ${verse.Chapter}, Verse ${verse.Verse}`;
-      return (
-        (chapterAndVerse.toLowerCase().includes(searchChapter.toLowerCase()) ||
-        verse._id.toLowerCase().includes(searchVerse.toLowerCase()))
-      );
-    });
-    setDisplayedVerses(filteredVerses);
-  };
-
-  const handlePageChange = ({ selected }) => {
-    setPageNumber(selected);
-  };
-
-  const pageCount = Math.ceil(displayedVerses.length / versesPerPage);
-  const displayedVersesSlice = displayedVerses.slice(
-    pageNumber * versesPerPage,
-    (pageNumber + 1) * versesPerPage
-  );
-
-  const handleVerseClick = (verse) => {
-    setSelectedVerse(verse);
-  };
-
+  /** 
+   * !6.Render Content:
+   * ?In the component's render method, it displays the fetched Bhagavad Gita verses.
+   * ?The title "Bhagavad Gita Verses" is displayed as an h1 element.
+   * *The verses are listed as li elements within an unordered list (ul).
+   * ?It maps over the verses state and displays each verse's chapter, verse number, and shloka (text).
+   */
   return (
-    <Container className="container" maxWidth="md">
-      <div>
-        <Typography variant="h3" gutterBottom>
-          Bhagavad Gita Verses
-        </Typography>
-        <div>
-          <input
-            type="text"
-            placeholder="Search by Chapter"
-            onChange={(e) => handleChapterInputChange(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Search by Verse ID"
-            onChange={(e) => handleVerseInputChange(e.target.value)}
-          />
-          <button onClick={handleSearch}>Search</button>
-        </div>
-      </div>
-      {displayedVersesSlice.map((verse) => (
-        <div
-          key={verse._id}
-          className="verse"
-          onClick={() => handleVerseClick(verse)}
-        >
-          <Typography variant="h5" gutterBottom>
-            Chapter {verse.Chapter}, Verse {verse.Verse}
-          </Typography>
-          <Typography variant="body1">{verse.Shloka}</Typography>
-          <Typography variant="body2" className="transliteration" gutterBottom>
-            {verse.Transliteration}
-          </Typography>
-        </div>
-      ))}
-
-      {selectedVerse && (
-        <div className="verse-detail">
-          <Typography variant="h5" gutterBottom>
-            Chapter {selectedVerse.Chapter}, Verse {selectedVerse.Verse}
-          </Typography>
-          <Typography variant="body1">{selectedVerse.Shloka}</Typography>
-          {/* Additional details */}
-        </div>
-      )}
-
-      <ReactPaginate
-        previousLabel={'Previous'}
-        nextLabel={'Next'}
-        breakLabel={'...'}
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageChange}
-        containerClassName={'pagination'}
-        activeClassName={'active'}
-      />
-    </Container>
+    <div>
+      <h1>Bhagavad Gita Verses</h1>
+      <ul>
+        {verses.map((verse) => (
+          <li key={verse._id}>
+            <strong>Chapter {verse.Chapter}, Verse {verse.Verse}:</strong> {verse.Shloka}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
